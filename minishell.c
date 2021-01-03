83100cbd510b8e71ea023877323b695bbfe752c3
LblCH <nicio@ya.ru>
2021-01-03 08:12:22 +0300

    one

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: ztawanna <ztawanna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/02 23:08:34 by ztawanna          #+#    #+#             */
-/*   Updated: 2021/01/03 06:03:02 by ztawanna         ###   ########.fr       */
+/*   Updated: 2021/01/03 07:23:35 by ztawanna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,8 @@ int main(int argc, char **argv, char **envp)
 
 	(void)argc;
 	(void)argv;
-
 	shell.env = envp;
-	shell.command = ft_strdup("111");
+	ft_putstr_fd("💀 \033[36miMiNishell ⇥ \033[0m", 1);
 	ft_parcer(&shell);
 	printf("\n%s\n", shell.command);
 	return (0);
--- parcer.c
+++ parcer.c
@@ -6,24 +6,48 @@
 /*   By: ztawanna <ztawanna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/03 04:15:50 by ztawanna          #+#    #+#             */
-/*   Updated: 2021/01/03 06:57:55 by ztawanna         ###   ########.fr       */
+/*   Updated: 2021/01/03 07:26:37 by ztawanna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int quotes_cl(char *line)
+{
+	int open;
+
+	open = 0;
+	while(line)
+	{
+		if (*line == '\'' && open == 0)
+			open = 1;
+		else if (*line == '\"' && open == 0)
+			open = 2;
+		else if (*line == '\'' && open == 1)
+			open = 0;
+		else if (*line == '\"' && open == 2)
+			open = 0;
+		line++;
+	}
+	return (open);
+}
+
 int ft_parcer(t_shell *shell)
 {
 	char *line;
 
-	ft_putstr_fd("💀 \033[36mMiNishell ⇥ \033[0m", 1);
 	if(get_next_line(0, &line) < 0)
 		ft_putstr_fd("Failed GNL", 2);
 	while(ft_isspace(*line))
 		line++;
 	while(*line != '\0')
 	{
-		if(ft_strchr(line, '\'\"')
+		if((ft_strchr(line, '\'') || ft_strchr(line, '\"')) && quotes_cl(line))
+		{
+			ft_putstr_fd("Non couple quotes.", 2);
+			return(0);
+		}
+
 	}
 	return (0);
 }
