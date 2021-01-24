c109a258b1195820b058cd67abf1bdbd59455a2a
cdrennan <r9375806222@gmail.com>
2021-01-24 20:37:09 +0300

    exit fixed

--- buildins/exit.c
+++ buildins/exit.c
@@ -6,7 +6,7 @@
 /*   By: cdrennan <cdrennan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 23:42:50 by cdrennan          #+#    #+#             */
-/*   Updated: 2021/01/22 20:20:26 by cdrennan         ###   ########.fr       */
+/*   Updated: 2021/01/24 20:34:55 by cdrennan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,14 @@ void		ft_exit(t_shell *shell)
 {
 	shell->exit = 1;
 	ft_putstr_fd("exit\n", 2);
+	if (shell->start->args[1] && shell->start->args[2])
+	{
+		shell->ret = 1;
+		shell->exit = 0;
+		ft_putstr_fd(": ", 2);
+		ft_putstr_fd(shell->start->args[0], 2);
+		ft_putendl_fd(": too many arguments", 2);
+	}
 	if (shell->start->args[1])
 	{
 		if (arg_is_digit(shell->start->args[1]))
@@ -38,7 +46,7 @@ void		ft_exit(t_shell *shell)
 		{
 			shell->ret = 255;
 			ft_putstr_fd(": ", 2);
-			ft_putstr_fd(shell->start->args[1], 2);
+			ft_putstr_fd(shell->start->args[0], 2);
 			ft_putendl_fd(": numeric argument required", 2);
 		}
 	}
