2468ba80d2a43b416c8fd4e9a5c0fc120203823e
cdrennan <r9375806222@gmail.com>
2021-02-26 21:06:21 +0300

    unset

--- buildins/unset.c
+++ buildins/unset.c
@@ -6,7 +6,7 @@
 /*   By: cdrennan <cdrennan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 12:49:23 by cdrennan          #+#    #+#             */
-/*   Updated: 2021/02/26 20:58:53 by cdrennan         ###   ########.fr       */
+/*   Updated: 2021/02/26 21:00:54 by cdrennan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,7 @@ char		**recreate_env_array(char **env, int delete)
 int	ft_unset(t_shell *shell, char *arg)
 {
 	int i;
+	char **tmp;
 
 	i = 0;
 	if (!arg)
@@ -59,7 +60,9 @@ int	ft_unset(t_shell *shell, char *arg)
 	{
 		if (ft_strncmp(shell->env[i], arg, ft_strlen(arg)) == 0)
 		{
-			shell->env = recreate_env_array(shell->env, i);
+			tmp = shell->env;
+			shell->env = recreate_env_array(tmp, i);
+			free_tab(tmp);
 			return (0);
 		}
 		i++;
