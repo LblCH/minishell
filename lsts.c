0044268507eeb31bbec054e4b3b70489b0b63e4d
cdrennan <r9375806222@gmail.com>
2021-03-05 21:58:18 +0300

    efef

--- lsts.c
+++ lsts.c
@@ -6,7 +6,7 @@
 /*   By: ztawanna <ztawanna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 17:03:42 by ztawanna          #+#    #+#             */
-/*   Updated: 2021/03/05 14:23:15 by cdrennan         ###   ########.fr       */
+/*   Updated: 2021/03/05 21:56:03 by cdrennan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,15 +44,15 @@ void		add_token(t_shell *shell, t_token *token, char *line)
 		token->fd_in = shell->fd;
 	shell->fd = -1;
 	shell->fd_type = 0;
-	printf("--------------\nToken added\n");
-	printf("fd_in: %d fd_out: %d\n", token->fd_in, token->fd_out);
-	i = 0;
-	while (token->args && token->args[i])
-	{
-		printf("arg %d: %s\n", i, token->args[i]);
-		i++;
-	}
-	printf("-------§-------\n");
+//	printf("--------------\nToken added\n");
+//	printf("fd_in: %d fd_out: %d\n", token->fd_in, token->fd_out);
+//	i = 0;
+//	while (token->args && token->args[i])
+//	{
+//		printf("arg %d: %s\n", i, token->args[i]);
+//		i++;
+//	}
+//	printf("-------§-------\n");
 }
 
 t_token		*new_token(void)
